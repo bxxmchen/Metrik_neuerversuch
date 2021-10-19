bc19f9d75a39d5a986f3190ec83445166df7cd04
bxxmchen <u36651@hs-harz.de>
2021-10-20 00:43:02 +0200

    Version 1.0.1 two squares

--- src/de/medieninformatik/softwaretechnik/CanvasController.java
+++ src/de/medieninformatik/softwaretechnik/CanvasController.java
@@ -3,7 +3,7 @@ package de.medieninformatik.softwaretechnik;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 /**
  * @author Carolin Baum, m29137
@@ -20,14 +20,14 @@ public class CanvasController implements MouseListener {
     Canvas canvas;
 
     /**
-     * The method mouseClicked counts the amount of mouse clicks and draws the circles.
+     * The method mouseClicked counts the amount of mouse clicks and draws the squares.
      * @param e event
      */
     @Override
     public void mouseClicked(MouseEvent e) {
         canvas = (Canvas) e.getSource();
         Graphics g = canvas.getGraphics();
-        Ellipse2D ellipse2d;
+        Rectangle2D rectangle2D;
         Graphics2D g2d = (Graphics2D)g;
         g2d.setColor(Color.ORANGE);
 
@@ -36,20 +36,20 @@ public class CanvasController implements MouseListener {
 
             //counts the quantity of double clicks
             switch (counter){
-                //first click, first circle
+                //first click, first square
                 case 0:
                     x1 = e.getX();
                     y1 = e.getY();
-                    ellipse2d = new Ellipse2D.Float(x1-20.0F, y1-20.0F, 40.0F, 40.0F);
-                    g2d.fill(ellipse2d);
+                    rectangle2D = new Rectangle2D.Float(x1-20.0F, y1-20.0F, 40.0F, 40.0F);
+                    g2d.fill(rectangle2D);
                     counter++;
                     break;
-                //second click, second circle
+                //second click, second square
                 case 1:
                     x2 = e.getX();
                     y2 = e.getY();
-                    ellipse2d = new Ellipse2D.Float(x2-20.0F, y2-20.0F, 40.0F, 40.0F);
-                    g2d.fill(ellipse2d);
+                    rectangle2D = new Rectangle2D.Float(x2-20.0F, y2-20.0F, 40.0F, 40.0F);
+                    g2d.fill(rectangle2D);
                     counter++;
                     break;
                 //third click, clear canvas
