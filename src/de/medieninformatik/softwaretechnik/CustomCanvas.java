673bbf349abc7dded4d96685fa54608c7433bcb4
bxxmchen <u36651@hs-harz.de>
2021-10-20 00:49:55 +0200

    Version 1.1.0 two circles and menu for background color

--- src/de/medieninformatik/softwaretechnik/CustomCanvas.java
+++ src/de/medieninformatik/softwaretechnik/CustomCanvas.java
@@ -19,4 +19,12 @@ public class CustomCanvas extends Canvas {
         setSize(850, 550);
         setBackground(Color.WHITE);
     }
+
+    /**
+     * The method changeBackground is responsible for a new background color.
+     * @param c color
+     */
+    public void changeBackground(Color c){
+        setBackground(c);
+    }
 }
